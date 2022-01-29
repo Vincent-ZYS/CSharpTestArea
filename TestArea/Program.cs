eb20e1597a05605c8dadd6c0b4eeeb72a67d172f
Vincent-ZYS <1654209516@qq.com>
2022-01-29 15:31:40 +0800

    Update the new test function in Promgram.cs

--- TestArea/Program.cs
+++ TestArea/Program.cs
@@ -19,14 +19,12 @@ namespace TestArea
 
             /*Test02*/
             /*通过Expression调用实体的方法*/
-            var _serviceProvider = new ServiceProvider();
-            var service = _serviceProvider.GetService(_serviceProvider);
-            DynamicMethodExecutor dynamicMethodRun = new DynamicMethodExecutor(service, "Run");
-            var redata = dynamicMethodRun.Execute(service, null);
-            Console.WriteLine(redata);
+            //var _serviceProvider = new ServiceProvider();
+            //var service = _serviceProvider.GetService(_serviceProvider);
+            //DynamicMethodExecutor dynamicMethodRun = new DynamicMethodExecutor(service, "Run");
+            //var redata = dynamicMethodRun.Execute(service, null);
+            //Console.WriteLine(redata);
 
-            Expression convertExpr = Expression.Convert(Expression.Constant(5.5), typeof(Int16));
-            Console.WriteLine(convertExpr.ToString());             
             /*Test03*/
             /*通过表达式获取成员属性*/
             //PersonModel pm = new PersonModel();
@@ -34,6 +32,7 @@ namespace TestArea
 
             //Simple_Test
             //SimpleTest.TestMethod01();
+            SimpleTest.TestMethod02();
         }
     }
 
@@ -48,6 +47,17 @@ namespace TestArea
             MethodInfo subStr = strType.GetMethod("Substring", new Type[] { typeof(int), typeof(int) });
             object result = subStr.Invoke("Hello, World!", new object[] { 7, 5 });
             Console.WriteLine("{0} returned /{1}/", subStr, result);
+        }
+
+        /// <summary>
+        /// 下面的代码示例演示如何创建表示类型转换运算的表达式
+        /// </summary>
+        public static void TestMethod02()
+        {
+            Expression convertExpr = Expression.Convert(Expression.Constant(5.5), typeof(Int16));
+            Console.WriteLine(convertExpr.ToString());
+
+            Console.WriteLine(Expression.Lambda<Func<Int16>>(convertExpr).Compile()());
         }
     }
 }
